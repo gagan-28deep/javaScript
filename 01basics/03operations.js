79c639897f9659f3f5e550c39c896ebe9066b0fb
Gagandeep Singh <66797838+gagan-28deep@users.noreply.github.com>
2023-10-11 19:04:08 +0000

    operations added

---
+++ 01basics/03operations.js
@@ -0,0 +1,14 @@
+// console.log(1 + "2")  // 12
+
+// console.log("1" + 2)  // 12
+
+// console.log("1" + 2 + 2)  // 122
+// If string is first, all will be treated as string
+
+// console.log(1 + 2 + "2")   // 32
+// If string is last , number is evaluated first like 1 + 2 -> 3
+
+// console.log(+true)  // 1
+// console.log(true+) // error
+
+// console.log(+"") // 0
